6360b09be62e1da0410ee9ab58f07522049be627
Grzegorz Kupczyk <hagisek1998@gmail.com>
2017-11-08 17:47:30 +0100

    First real application!

---
+++ NavigationDrawer/app/src/main/java/pl/kgrzeg/navigationdrawer/MainActivity.java
@@ -0,0 +1,28 @@
+package pl.kgrzeg.navigationdrawer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static String version = "v0.1";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ((TextView)findViewById(R.id.textViewVersion)).setText(version);
+    }
+
+    public void doLogin(View view) {
+        Intent i = new Intent(this, UserPrivateActivity.class);
+        startActivity(i);
+    }
+
+    public void doRegister(View view) {
+    }
+}
