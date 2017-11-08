f5d1f733efb06f4e8eceeb10d99f70bd0a5c1c7f
Grzegorz Kupczyk <hagisek1998@gmail.com>
2017-11-08 18:46:45 +0100

    Add first fragment to the menu (mechanics)

---
+++ NavigationDrawer/app/src/main/java/pl/kgrzeg/navigationdrawer/AwariaItem.java
@@ -0,0 +1,54 @@
+package pl.kgrzeg.navigationdrawer;
+
+/**
+ * Created by course on 08/11/2017.
+ */
+
+public class AwariaItem {
+    private String title;
+    private String description;
+    private String carModel;
+    private String photoUrl;
+
+    public AwariaItem() {
+    }
+
+    public AwariaItem(String title, String description, String carModel, String photoUrl) {
+        this.title = title;
+        this.description = description;
+        this.carModel = carModel;
+        this.photoUrl = photoUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+}
