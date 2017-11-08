4ee61fc5df1cae03a9df254ea03408ea25e9811c
Grzegorz Kupczyk <hagisek1998@gmail.com>
2017-11-08 19:45:23 +0100

    Finished adapter!

--- NavigationDrawer/app/src/main/java/pl/kgrzeg/navigationdrawer/MyAwariaItemRecyclerViewAdapter.java
+++ NavigationDrawer/app/src/main/java/pl/kgrzeg/navigationdrawer/MyAwariaItemRecyclerViewAdapter.java
@@ -5,7 +5,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import pl.kgrzeg.navigationdrawer.AwariaItemFragment.OnListFragmentInteractionListener;
 import pl.kgrzeg.navigationdrawer.dummy.DummyContent.DummyItem;
@@ -31,22 +34,13 @@ public class MyAwariaItemRecyclerViewAdapter extends RecyclerView.Adapter<MyAwar
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
-        //Picasso.with(context).load("http://i.imgur.com/DvpvklR.png").into(imageView);
-
+        // Get the current element
         holder.mItem = mValues.get(position);
-        holder.mIdView.setText(mValues.get(position).id);
-        holder.mContentView.setText(mValues.get(position).content);
 
-        holder.mView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (null != mListener) {
-                    // Notify the active callbacks interface (the activity, if the
-                    // fragment is attached to one) that an item has been selected.
-                    mListener.onListFragmentInteraction(holder.mItem);
-                }
-            }
-        });
+        holder.titleTextView.setText(mValues.get(position).getTitle());
+        holder.carModelTextView.setText(mValues.get(position).getCarModel());
+
+        Picasso.with(ctx).load(mValues.get(position).getPhotoUrl()).into(holder.photoImageView);
     }
 
     @Override
@@ -55,21 +49,24 @@ public class MyAwariaItemRecyclerViewAdapter extends RecyclerView.Adapter<MyAwar
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        public final View mView;
-        public final TextView mIdView;
-        public final TextView mContentView;
-        public DummyItem mItem;
+        public View mView;
+        public ImageView photoImageView;
+        public TextView titleTextView, carModelTextView;
+        public AwariaItem mItem;
 
         public ViewHolder(View view) {
             super(view);
             mView = view;
-            mIdView = (TextView) view.findViewById(R.id.id);
-            mContentView = (TextView) view.findViewById(R.id.content);
+
+            photoImageView = view.findViewById(R.id.imageViewPhoto);
+            titleTextView = view.findViewById(R.id.textViewTitle);
+            carModelTextView = view.findViewById(R.id.textViewCarModel);
+
         }
 
         @Override
         public String toString() {
-            return super.toString() + " '" + mContentView.getText() + "'";
+            return super.toString() + " '" + titleTextView.getText() + "'";
         }
     }
 }
